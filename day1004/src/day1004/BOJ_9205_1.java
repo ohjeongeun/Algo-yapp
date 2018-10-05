7d43712fe7ae640f403ff28721da0462ec8f5ab7
ojeongeuni <zyzm12@gmail.com>
2018-10-05 14:30:30 +0900

    맥주 마시며 걸어가기성공

--- day1004/src/day1004/BOJ_9205_1.java
+++ day1004/src/day1004/BOJ_9205_1.java
@@ -1,24 +1,23 @@
-/*package day1004;
+package day1004;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BOJ_9205 {
+public class BOJ_9205_1 {
 	static int T;
 	static int N;
-	static int map[][];
-	static int visited[][];
+	static int visited[];
 	static Queue<Node> q;
 	
-	static int dx[] = {50, 0, -50, 0};
-	static int dy[] = {0, 50, 0, -50};
 	static int fentaY;
 	static int fentaX;
+	
+	static LinkedList<Node> list;
+	static boolean flag;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
@@ -27,89 +26,79 @@ public class BOJ_9205 {
 			N = Integer.parseInt(br.readLine()); //편의점 개수
 						
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			list = new LinkedList<>();
 			
 			int homeY = Integer.parseInt(st.nextToken());
 			int homeX = Integer.parseInt(st.nextToken());
+			list.add(new Node(homeY, homeX));
 			
 			
-			ArrayList<Node> list = new ArrayList<>();
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				int y = Integer.parseInt(st.nextToken());
 				int x = Integer.parseInt(st.nextToken());
-				list.add(new Node(y, x, 0));
+				list.add(new Node(y, x));
 			}
 			
 			st = new StringTokenizer(br.readLine());
 			fentaY = Integer.parseInt(st.nextToken());
 			fentaX = Integer.parseInt(st.nextToken());
+			list.add(new Node(fentaY, fentaX));
 			
-			map = new int[fentaY+1][fentaX+1];
-			visited = new int[fentaY+1][fentaX+1];
-			
-			for(int i=0; i<N; i++) {
-				int y = list.get(i).y;
-				int x = list.get(i).x;
-				map[y][x]=2; //편의점 표식
-			}
+			visited = new int[N+2];
 			
 			q = new LinkedList<>();
+			flag=false;
 			bfs(homeY,homeX);
-			
+			if(flag==true) {
+				System.out.println("happy");
+			}else {
+				System.out.println("sad");
+			}
 		}
 	}
 	
+	public static int len(Node a, Node b) {
+		int result=0;
+		result = Math.abs(a.x-b.x)+Math.abs(a.y-b.y);
+		return result;
+	}
 	
 	public static void bfs(int y, int x) {
-		q.add(new Node(y, x, 20));
-		visited[y][x]=1;
+		q.add(new Node(y, x));
+		visited[0]=1;
 		
 		while(!q.isEmpty()) {
-			int cury = q.peek().y;
-			int curx = q.peek().x;
-			int curnum = q.peek().num;
-			
-			if(cury==fentaY && curx==fentaX) {
-				if(curnum<0) {
-					System.out.println("sad");
-				}else {
-					System.out.println("happy");
-				}
+			Node cur = q.remove();
+			if(cur.equals(list.getLast())) {
+				flag=true;
+				return;
 			}
 			
-			
-			q.remove();
-			
-			for(int i=0; i<4; i++) {
-				int ny = cury+dy[i];
-				int nx = curx+dx[i];
+			for(int i=0; i<N+2; i++) {
+				Node p = list.get(i);
 				
-				if(ny<0 || nx<0 || ny>fentaY || nx>fentaX) continue;
+				if(visited[i]==1) continue;
 				
-				if(visited[ny][nx]==1) continue;
+				//현재위치와 편의점까지의 거리
+				int distance = len(cur, p);
 				
-				if(map[ny][nx]==2) {
-					q.add(new Node(ny, nx, 20));
-					visited[ny][nx]=1;
+				if(distance<=1000) {
+					q.add(p);
+					visited[i]=1;
 				}
 				
-				if(map[ny][nx]==0) {
-					q.add(new Node(ny, nx, curnum-1));
-					visited[ny][nx] = 1;
-				}
+				
 			}
-			
 		}
 	}
 }
 
 class Node {
-	int y, x, num;
-	public Node(int y, int x, int num) {
+	int y, x;
+	public Node(int y, int x) {
 		this.y = y;
 		this.x = x;
-		this.num = num;
 	}
 	
 }
-*/
